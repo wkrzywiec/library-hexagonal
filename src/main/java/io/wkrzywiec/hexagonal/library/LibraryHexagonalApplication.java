c19b58e5f5f4cc908f6f726b8d931ad89bc266ec
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2020-04-29 20:44:08 +0200

    init commit

---
+++ src/main/java/io/wkrzywiec/hexagonal/library/LibraryHexagonalApplication.java
@@ -0,0 +1,13 @@
+package io.wkrzywiec.hexagonal.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/wkrzywiec/hexagonal/library/LibraryHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wkrzywiec.hexagonal.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
