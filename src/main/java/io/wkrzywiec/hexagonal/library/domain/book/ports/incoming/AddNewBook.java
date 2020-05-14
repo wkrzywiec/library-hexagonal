31db7bf37ae0f62ee8abdb9457aa3e9a8234f0f9
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2020-05-14 13:45:02 +0200

    rename model folder to dto

--- src/main/java/io/wkrzywiec/hexagonal/library/domain/book/ports/incoming/AddNewBook.java
+++ src/main/java/io/wkrzywiec/hexagonal/library/domain/book/ports/incoming/AddNewBook.java
@@ -1,6 +1,6 @@
 package io.wkrzywiec.hexagonal.library.domain.book.ports.incoming;
 
-import io.wkrzywiec.hexagonal.library.domain.book.model.ExternalBookIdDTO;
+import io.wkrzywiec.hexagonal.library.domain.book.dto.ExternalBookIdDTO;
 
 public interface AddNewBook {
     void handle(ExternalBookIdDTO externalBookIdDTO);
--- src/main/java/io/wkrzywiec/hexagonal/library/domain/book/ports/incoming/GetBookDetails.java
+++ src/main/java/io/wkrzywiec/hexagonal/library/domain/book/ports/incoming/GetBookDetails.java
@@ -1,6 +1,6 @@
 package io.wkrzywiec.hexagonal.library.domain.book.ports.incoming;
 
-import io.wkrzywiec.hexagonal.library.domain.book.model.BookDetailsDTO;
+import io.wkrzywiec.hexagonal.library.domain.book.dto.BookDetailsDTO;
 
 public interface GetBookDetails {
     BookDetailsDTO handle(String bookId);
