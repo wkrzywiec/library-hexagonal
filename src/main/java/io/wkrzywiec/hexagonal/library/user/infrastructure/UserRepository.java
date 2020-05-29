fa0e83ad927ed16a1a648ce89c1982acde17d5f9
wkrzywiec <wojciechkrzywiec@gmail.com>
2020-05-29 08:56:16 +0200

    UserCommandController

--- src/main/java/io/wkrzywiec/hexagonal/library/user/infrastructure/UserRepository.java
+++ src/main/java/io/wkrzywiec/hexagonal/library/user/infrastructure/UserRepository.java
@@ -2,6 +2,8 @@ package io.wkrzywiec.hexagonal.library.user.infrastructure;
 
 import io.wkrzywiec.hexagonal.library.user.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 }
